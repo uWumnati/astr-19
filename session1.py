5ec06374791330187e27b53e52ccb6f40a9b393b
Peace2112 <146383827+Peace2112@users.noreply.github.com>
2023-10-19 21:53:15 -0700

    Delete session1.py

--- session1.py
+++
@@ -1,4 +0,0 @@
-def main():
-  print('My name is Peter Jiang.\nMy favorite movie is Up as it\'s something that was super sweet and fun to watch.\nMy favorite food is seafood as fish has amazing flavors.');
-if __name__ == "__main__":
-  main()
