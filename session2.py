9223e4de0cc7b1be22063ae32e57cd826fb2009f
Peace2112 <146383827+Peace2112@users.noreply.github.com>
2023-10-19 21:34:55 -0700

    Update session2.py

--- session2.py
+++ session2.py
@@ -1,6 +1,9 @@
-a = 5.0 + 2.0
-b = 5 - 2
-c = 5.0*2
-print(a,type(a))
-print(b,type(b))
-print(c,type(c))
+def main():
+	a = 5.0 + 2.0
+	b = 5 - 2
+	c = 5.0*2
+	print(a,type(a))
+	print(b,type(b))
+	print(c,type(c))
+if __name__ == "__main__":
+	main()
