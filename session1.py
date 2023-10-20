66452455614cd65c7bcccf5dc63b25a8400211df
Peace2112 <146383827+Peace2112@users.noreply.github.com>
2023-10-19 21:34:11 -0700

    Update session1.py

--- session1.py
+++ session1.py
@@ -1,1 +1,4 @@
-print('My name is Peter Jiang.\nMy favorite movie is Up as it\'s something that was super sweet and fun to watch.\nMy favorite food is seafood as fish has amazing flavors.');
+def main():
+  print('My name is Peter Jiang.\nMy favorite movie is Up as it\'s something that was super sweet and fun to watch.\nMy favorite food is seafood as fish has amazing flavors.');
+if __name__ == "__main__":
+  main()
